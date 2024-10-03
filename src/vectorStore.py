24781ec1ba619988fd45016b8e85cbedd75ea626
Wei-Ju Chen <wxc276@student.bham.ac.uk>
2024-10-04 00:54:33 +0800

    fix vectorStore.py

--- src/vectorStore.py
+++ src/vectorStore.py
@@ -3,8 +3,10 @@ import os
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Qdrant
 import qdrant_client
-
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 client = qdrant_client.QdrantClient(
@@ -22,6 +24,56 @@ vectorstore = Qdrant(
     embeddings=embeddings,
 )
 
+# import sys
+
+# print(sys.path)
+
+# load environment variable
+load_dotenv()
+
+#  Created function to load multiple PDFs
+
+pdf_folder_path = os.getenv("PDF_FOLDER_PATH")
+
+
+def load_multiple_pdfs(pdf_folder_path):  # ***
+    docs = []
+    # 檢查資料夾是否存在
+    if not os.path.exists(pdf_folder_path):
+        print(f"Error: The directory {pdf_folder_path} does not exist.")
+        return []
+    for file_name in os.listdir(pdf_folder_path):  # ***
+        if file_name.endswith(".pdf"):  # *** Check if file is a PDF
+            file_path = os.path.join(
+                pdf_folder_path, file_name
+            )  # *** Get full path of the file
+            loader = PyPDFLoader(file_path)  # *** Load the PDF using PyPDFLoader
+            docs.extend(loader.load())  # *** Add loaded documents to docs list
+    return docs  # *** Return the full list of documents
+
+
+# pdf_docs = load_multiple_pdfs(pdf_folder_path)
+# print(pdf_docs)  # 這將打印所有 PDF 檔案的路徑
+
+
+def get_chunk(docs):
+    # split your docs into texts chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=50,
+        separators=["\n", "。", "！", "？", "，", "、", ""],
+    )
+    chunks = text_splitter.split_documents(docs)
+    return chunks
+
+    # load my PDFs
+
+
+pdf_folder_path = os.getenv("PDF_FOLDER_PATH")
+docs = load_multiple_pdfs(pdf_folder_path)
+
+texts = get_chunk(docs)
+vectorstore.add_texts(texts)
 
 # Create a Qdrant instance
 # vectorstore = QdrantVectorStore(
